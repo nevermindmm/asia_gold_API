6eaeeef4d8fe58a01198b589458722a167c2765e
folkspch <folk_spch@hotmail.com>
2023-04-09 23:12:59 +0700

    update

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.post("/getProdList", (req, res) => {
 
 app.post("/addSales", async (req, res) => {
   let { date, platform, total_sales, prod_list } = req.body;
-  let prod_list_string
+  let prod_list_string = JSON.stringify(prod_list)
   for (let i = 0; i < prod_list.length; i++) {
     let temp_weight = prod_list[i].weight
     let weight = parseFloat(temp_weight.split(/[()]/)[1])
@@ -91,17 +91,26 @@ app.post("/addSales", async (req, res) => {
     dbCon.query(`SELECT remain FROM product_list WHERE type =? AND pattern=? AND weight_th=? AND TRIM(weight)=?`, [prod_list[i].type, prod_list[i].pattern, weight_th, weight], (error, results, fields) => {
       if (error) throw error;
       if (prod_list[i].qty > results[0].remain) {
-        let prod_conflict = {...prod_list[i]}
+        let prod_conflict = { ...prod_list[i] }
         prod_conflict.stock_remain = results[0].remain
-        return res.status(400).send(prod_conflict);
+        return res.status(201).send(prod_conflict);
       }
       if (i == prod_list.length - 1) {
-        prod_list_string = JSON.stringify(prod_list)
         if (date && platform && total_sales && prod_list) {
-          dbCon.query(`INSERT INTO sales (platform,prod_list,sales_date,total_sales) VALUES(?,?,?,?)`, [platform, prod_list, date, total_sales], (error, results, fields) => {
-            if (error) throw error;
-            res.status(200).send({ message: 'Success' });
-          });
+          for (let j = 0; j < prod_list.length; j++) {
+            let temp_weight = prod_list[j].weight
+            let weight = parseFloat(temp_weight.split(/[()]/)[1])
+            let weight_th = temp_weight.split(/[()]/)[0]
+            dbCon.query(`UPDATE product_list SET remain = remain - ? WHERE type =? AND pattern=? AND weight_th=? AND TRIM(weight)=? AND remain >= ?`, [prod_list[j].qty, prod_list[j].type, prod_list[j].pattern, weight_th, weight, prod_list[j].qty], (error, results, fields) => {
+              if (error) throw error;
+              if (j == prod_list.length - 1) {
+                dbCon.query(`INSERT INTO sales (platform,prod_list,sales_date,total_sales) VALUES(?,?,?,?)`, [platform, prod_list_string, date, total_sales], (error, results, fields) => {
+                  if (error) throw error;
+                  res.status(200).send({ message: 'Success' });
+                });
+              }
+            });
+          }
         }
       }
     })
@@ -254,7 +263,7 @@ app.post("/editProdData", (req, res) => {
 })
 app.post("/delProd", (req, res) => {
   let { id } = req.body
-  console.log(id)
+  // console.log(id)
   if (id) {
     dbCon.query(`DELETE FROM product_list WHERE id = ?`, [id], (error, results, fields) => {
       if (error) throw error;
@@ -264,7 +273,7 @@ app.post("/delProd", (req, res) => {
 })
 app.post("/addProd", (req, res) => {
   let { type, pattern, weight, remain } = req.body
-  console.log(req.body)
+  // console.log(req.body)
   if (type && pattern && weight) {
     let weight_th
     let splitValue = weight.split('(');
