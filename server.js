e12c81e52576142f56572b5840e6c58e36373d84
folkspch <folk_spch@hotmail.com>
2023-05-10 15:43:08 +0700

    nf

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 4000
 // homepage route
 app.get("/", (req, res) => {
   return res.send({
